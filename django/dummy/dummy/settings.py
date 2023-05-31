98fa3ae80c5735267cb553832d6ea82e29272368
Stef Piatek <s.piatek@ucl.ac.uk>
2023-05-31 17:48:24 +0100

    Try temporarily allowing all hosts

--- django/dummy/dummy/settings.py
+++ django/dummy/dummy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*0-tvz^cb!gtiyo@oz-(qi3i$a(7dt!wv%pmue!h^bmr7h_d^=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
