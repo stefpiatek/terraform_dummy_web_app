e985ae9ee28a6c0d7f9e74be4cdf249dfd404429
Stef Piatek <s.piatek@ucl.ac.uk>
2023-05-31 17:26:59 +0100

    Add https security rule

--- main.tf
+++ main.tf
@@ -117,3 +117,17 @@ resource "azurerm_network_security_rule" "sp_dummy_app" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.sp_dummy_app.name
 }
+
+resource "azurerm_network_security_rule" "sp_dummy_app" {
+  name                        = "allow-https"
+  priority                    = 443
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.sp_dummy_app.name
+}
