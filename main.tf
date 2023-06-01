bf9a09ac1240908f971e86bd68052f1a7178b3b2
Stef Piatek <s.piatek@ucl.ac.uk>
2023-06-01 11:53:50 +0100

    Remove http allow rules

--- main.tf
+++ main.tf
@@ -101,31 +101,3 @@ resource "azurerm_network_security_group" "sp_dummy_app" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-
-resource "azurerm_network_security_rule" "http" {
-  name                        = "allow-http"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.sp_dummy_app.name
-}
-
-resource "azurerm_network_security_rule" "https" {
-  name                        = "allow-https"
-  priority                    = 443
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.sp_dummy_app.name
-}
