1dbde96056d4d6a90cc94c3da2769924c89709d0
Stef Piatek <s.piatek@ucl.ac.uk>
2023-06-01 14:46:12 +0100

    Allow app to connect to sqlserver

--- main.tf
+++ main.tf
@@ -12,14 +12,6 @@ resource "random_pet" "storage_name" {
   separator = ""
 }
 
-resource "azurerm_storage_account" "sp_dummy_app" {
-  name                     = random_pet.storage_name.id
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
 resource "random_pet" "sql_name" {
   prefix = var.sql_server_name_prefix
 }
@@ -52,7 +44,40 @@ resource "azurerm_mssql_database" "sp_dummy_app" {
 }
 
 
+# create virtual network 
+resource "azurerm_virtual_network" "sp_dummy_app" {
+  name                = "dummy-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
 
+resource "azurerm_subnet" "sp_dummy_app" {
+  name                 = "test-app-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.sp_dummy_app.name
+  address_prefixes       = ["10.0.1.0/24"]
+  service_endpoints    = [ "Microsoft.Sql" ]
+  # required delegation to server farms
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+        actions = [
+            "Microsoft.Network/virtualNetworks/subnets/action",
+          ]
+        name = "Microsoft.Web/serverFarms"
+      }
+  }
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sp_dummy_app" {
+  name      = "sql-dummy-vnet-rule"
+  server_id = azurerm_mssql_server.sp_dummy_app.id
+  subnet_id = azurerm_subnet.sp_dummy_app.id
+}
+
+# Deploy application
 resource "azurerm_service_plan" "sp_dummy_app" {
   name                = "sp_dummy_service_plan"
   resource_group_name = azurerm_resource_group.rg.name
@@ -66,6 +91,7 @@ resource "azurerm_linux_web_app" "sp_dummy_app" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.sp_dummy_app.location
   service_plan_id     = azurerm_service_plan.sp_dummy_app.id
+  virtual_network_subnet_id = azurerm_subnet.sp_dummy_app.id
   https_only = true
   logs {
     application_logs {
@@ -86,7 +112,7 @@ resource "azurerm_linux_web_app" "sp_dummy_app" {
   }
   app_settings = {
     "DJANGO_SECRET_KEY" = var.django_secret,
-    "DB_NAME" = azurerm_mssql_server.sp_dummy_app.name,
+    "DB_NAME" = azurerm_mssql_database.sp_dummy_app.name,
     "DB_USER" = azurerm_mssql_server.sp_dummy_app.administrator_login,
     "DB_PASSWORD" = azurerm_mssql_server.sp_dummy_app.administrator_login_password,
     "DB_HOST" = azurerm_mssql_server.sp_dummy_app.fully_qualified_domain_name,
@@ -97,11 +123,4 @@ resource "azurerm_linux_web_app" "sp_dummy_app" {
 resource "azurerm_source_control_token" "sp_dummy_app" {
   type  = "GitHub"
   token = var.gh_token
-}
-
-# Allow access to the webapp
-resource "azurerm_network_security_group" "sp_dummy_app" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
 }
